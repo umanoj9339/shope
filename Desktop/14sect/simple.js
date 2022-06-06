05da6315b85ed0b97f6a907a2e15aa9f4a1654bf
umanoj9336 <umanoj9339@gmail.com>
2022-06-06 19:22:02 +0530

    tryCatch

---
+++ Desktop/14sect/simple.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const simpleGit = require('simple-git');
+
+const git = simpleGit();
+
+app.get("/", (request, response) => {
+
+  response.send("Hello World!");
+  let k = function(){
+    "jjdklfd"
+}
+
+git.addRemote('um','https://github.com/umanoj9339/cc.git')
+
+git.add(['simple.js'])
+
+git.commit("tryCatch",k )
+git.push('origin','main')
+console.log("Done")
+});
+app.listen(3006);
+
+
+
+
+
